f68f6d6abf25e5db6ddffdc5a75602c32ae15c57
nitya32 <26nitya02@gmail.com>
2019-12-10 20:43:13 -0500

    update
    update

--- script/script.js
+++ script/script.js
@@ -2,5 +2,43 @@ $(".hamburger").click(function(){
 //  $(".header1").show();
     $(".header1").toggleClass('show');
   });
-
-//$(".hamburger").click(function(){$(".header1").show();});
+$(".hamburger").click(function(){
+//  $(".header1").show();
+    $(".header2").toggleClass('show');
+  });$(".hamburger").click(function(){
+//  $(".header1").show();
+    $(".header3").toggleClass('show');
+  });$(".hamburger").click(function(){
+//  $(".header1").show();
+    $(".header4").toggleClass('show');
+  });$(".hamburger").click(function(){
+//  $(".header1").show();
+    $(".header5").toggleClass('show');
+  });$(".hamburger").click(function(){
+//  $(".header1").show();
+    $(".header6").toggleClass('show');
+  });
+$(".circle1").click(function(){
+//  $(".header1").show();
+    $(".parallax").toggleClass('show');
+  });
+$(".circle2").click(function(){
+//  $(".header1").show();
+    $(".parallax").toggleClass('show');
+  });
+$(".circle3").click(function(){
+//  $(".header1").show();
+    $(".parallax").toggleClass('show');
+  });
+$(".circle4").click(function(){
+//  $(".header1").show();
+    $(".parallax").toggleClass('show');
+  });
+$(".circle5").click(function(){
+//  $(".header1").show();
+    $(".parallax").toggleClass('show');
+  });
+$(".circle6").click(function(){
+//  $(".header1").show();
+    $(".parallax").toggleClass('show');
+  });
