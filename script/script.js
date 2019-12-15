a4a3b8cb4fca58aa037695225dc5636be28a254e
nitya32 <26nitya02@gmail.com>
2019-12-15 02:29:30 -0500

    updated
    updated

--- script/script.js
+++ script/script.js
@@ -1,44 +1,37 @@
 $(".hamburger").click(function(){
-//  $(".header1").show();
-    $(".header1").toggleClass('show');
-  });
-$(".hamburger").click(function(){
-//  $(".header1").show();
-    $(".header2").toggleClass('show');
-  });$(".hamburger").click(function(){
-//  $(".header1").show();
-    $(".header3").toggleClass('show');
-  });$(".hamburger").click(function(){
-//  $(".header1").show();
-    $(".header4").toggleClass('show');
-  });$(".hamburger").click(function(){
-//  $(".header1").show();
-    $(".header5").toggleClass('show');
-  });$(".hamburger").click(function(){
-//  $(".header1").show();
-    $(".header6").toggleClass('show');
-  });
-$(".circle1").click(function(){
-//  $(".header1").show();
-    $(".parallax").toggleClass('show');
-  });
-$(".circle2").click(function(){
-//  $(".header1").show();
-    $(".parallax").toggleClass('show');
-  });
-$(".circle3").click(function(){
-//  $(".header1").show();
-    $(".parallax").toggleClass('show');
-  });
-$(".circle4").click(function(){
-//  $(".header1").show();
-    $(".parallax").toggleClass('show');
-  });
-$(".circle5").click(function(){
-//  $(".header1").show();
-    $(".parallax").toggleClass('show');
-  });
-$(".circle6").click(function(){
-//  $(".header1").show();
-    $(".parallax").toggleClass('show');
-  });
+    $(".header").toggleClass('show');
+});
+
+$(".circle").click(function(event){
+    var n = $(this).attr('class').slice(-1);
+    $(".parallax" + n).toggleClass('show');
+});
+
+$(".close-parallax").click(function(){
+   $(".rellax").removeClass("show"); 
+});
+
+$(document).click(function(event){
+    console.log($(this));
+    
+    //select the elements you want to close
+//    if( )
+//    $(".rellax.show").removeClass("show");
+})
+
+
+//
+//var parallax = document.getElementById("parallax");
+//var circle1 = document.getElementById("circle1");
+//circle1.onclick = function() {
+//    parallax.style.display = "block";
+//                             }
+
+//window.onclick = function(event) {
+//    
+//
+//    if(event.target == parallax) {
+//        parallax.style.display = "none";
+//    }
+//       
+//}
